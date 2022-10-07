b3cd3b6d7480b0c296ee81670a7440c8cec0f137
Andrey Vorotnikov <AndrewVorotnikov@yandex.ru>
2022-10-07 23:45:06 +0300

    Add daemon running

--- Vorotnikov.Andrey/lab1/daemon/daemon.cpp
+++ Vorotnikov.Andrey/lab1/daemon/daemon.cpp
@@ -4,7 +4,27 @@
 
 #include "daemon.h"
 
+#include <signal.h>
+
 extern Daemon Daemon::instance;
+
+void SignalHandler(const int sig)
+{
+    auto& instance = Daemon::instance;
+    switch(sig)
+    {
+    case SIGHUP:
+        instance.onReloadConfig_(instance.configPath_);
+        break;
+    case SIGTERM:
+        instance.onTerminate_();
+        instance.needWork_ = false;
+        instance.work_.notify_one();
+        break;
+    default:
+        break;
+    }
+}
 
 Daemon& Daemon::GetRef()
 {
@@ -18,7 +38,7 @@ Daemon::Daemon() : needWork_(false)
 bool Daemon::SetParams(
     const std::string& path,
     const std::function<void ()>& onWork,
-    const std::function<bool ()>& onReloadConfig,
+    const std::function<void (const std::string&)>& onReloadConfig,
     const std::function<void ()>& onTerminate,
     const std::chrono::seconds& duration
 )
@@ -31,5 +51,20 @@ bool Daemon::SetParams(
     onReloadConfig_ = onReloadConfig;
     onTerminate_ = onTerminate;
     duration_ = duration;
+    return true;
+}
+
+bool Daemon::Run()
+{
+    if (!needWork_)
+        return false;
+    while (true)
+    {
+        std::unique_lock lock(workMtx_);
+        work_.wait_for(lock, duration_, [daemon = this]() {return daemon->needWork_;});
+        if (!needWork_)
+            break;
+        onWork_();
+    }
     return true;
 }
