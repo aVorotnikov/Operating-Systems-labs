0c01c0222f19f6366ab10bd975ec0c08e04059fc
Andrey Vorotnikov <AndrewVorotnikov@yandex.ru>
2022-10-08 17:18:53 +0300

    Add config parser

---
+++ Vorotnikov.Andrey/lab1/config/config.h
@@ -0,0 +1,18 @@
+/// @file
+/// @brief Работа с конфигурацией
+/// @author Воротников Андрей
+
+#include "../copier/copier.h"
+
+/// @brief Пространство имён конфигурации
+namespace config
+{
+
+/// @brief Прочитать конфигурационный файл
+/// @param[in] path Путь к конфигурационному файлу
+/// @param[out] copyInfoList Список структур об информации для копирования файлов
+/// @param[out] duration Периодичность в секундах
+/// @return true если прочитать удалось, false - иначе
+bool ReadConfig(const std::string& path, std::vector<Copier::CopyInfo>& copyInfoList, unsigned& duration);
+
+} // namespace config
