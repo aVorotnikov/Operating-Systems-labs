1ab3bc6ed2109faffbd64e0ebe7e4f2d955554c6
Andrey Vorotnikov <AndrewVorotnikov@yandex.ru>
2022-10-09 20:08:35 +0300

    Add logging while work failed

--- Vorotnikov.Andrey/lab1/copier/copier.cpp
+++ Vorotnikov.Andrey/lab1/copier/copier.cpp
@@ -4,6 +4,7 @@
 
 #include "copier.h"
 
+#include <syslog.h>
 #include <algorithm>
 #include <filesystem>
 
@@ -47,29 +48,38 @@ bool Copier::Copy()
 {
     const std::set<std::string> othersSubdirs = {othersSubdirectory};
 
+    bool success = true;
     std::lock_guard lock(mtx_);
     for (const auto& [dstDirName, dstDirContent] : dstDirectories_)
     {
-        auto dstDirPath = std::filesystem::path(dstDirName);
-        std::filesystem::remove_all(dstDirPath);
-        std::filesystem::create_directory(dstDirPath);
-        for (const auto& [srcDirName, srcDirContent] : dstDirContent)
-            for (const auto& entry : std::filesystem::directory_iterator(srcDirName))
-            {
-                if (!entry.is_regular_file())
-                    continue;
-                auto path = entry.path();
-                auto subdirs = &othersSubdirs;
-                auto extensionIt = srcDirContent.find(path.extension());
-                if (srcDirContent.cend() != extensionIt)
-                    subdirs = &extensionIt->second;
-                for (const auto& subdir : *subdirs)
+        try
+        {
+            auto dstDirPath = std::filesystem::path(dstDirName);
+            std::filesystem::remove_all(dstDirPath);
+            std::filesystem::create_directory(dstDirPath);
+            for (const auto& [srcDirName, srcDirContent] : dstDirContent)
+                for (const auto& entry : std::filesystem::directory_iterator(srcDirName))
                 {
-                    auto subdirPath = dstDirPath / subdir;
-                    std::filesystem::create_directory(subdirPath);
-                    std::filesystem::copy_file(path, subdirPath / path.filename(), std::filesystem::copy_options::skip_existing);
+                    if (!entry.is_regular_file())
+                        continue;
+                    auto path = entry.path();
+                    auto subdirs = &othersSubdirs;
+                    auto extensionIt = srcDirContent.find(path.extension());
+                    if (srcDirContent.cend() != extensionIt)
+                        subdirs = &extensionIt->second;
+                    for (const auto& subdir : *subdirs)
+                    {
+                        auto subdirPath = dstDirPath / subdir;
+                        std::filesystem::create_directory(subdirPath);
+                        std::filesystem::copy_file(path, subdirPath / path.filename(), std::filesystem::copy_options::skip_existing);
+                    }
                 }
-            }
+        }
+        catch(...)
+        {
+            syslog(LOG_ERR, "Failed to copy files to '%s'", dstDirName.c_str());
+            success = false;
+        }
     }
-    return true;
+    return success;
 }
