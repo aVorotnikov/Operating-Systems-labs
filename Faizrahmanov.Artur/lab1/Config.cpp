a597239242cf83ac207c160dffc6c24dc2b2d782
Hembos <61026594+Hembos@users.noreply.github.com>
2022-10-22 13:38:48 +0300

    Lab1 by Faizrahmanov Artur
    * Initialize
    
    * Config parser added
    
    * File mover daemon added
    
    * Read config from command arguments and remove all files from folder
    
    * Build script added
    
    * Message that folder not found added
    
    * Instead of pair adde struct
    
    * The check that the config has been read has been moved to run
    
    * Added opening file descriptors
    
    * Minor bugs fixed
    
    * System(command) calls removed
    
    * Redone singleton
    
    * sleepDuration unsigned now
    
    * Files copy and not remove now

---
+++ Faizrahmanov.Artur/lab1/Config.cpp
@@ -0,0 +1,115 @@
+#include "Config.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+Config& Config::getInstance()
+{
+    static Config instance;
+    return instance;
+}
+
+void Config::setConfigPath(const std::string &configPath)
+{
+    this->configPath = std::filesystem::absolute(configPath);
+}
+
+bool Config::readConfig()
+{
+    sleepDuration = 0;
+    pathsAndFileExt.clear();
+    configReaded = false;
+
+    std::ifstream file;
+
+    file.open(configPath);
+
+    if (!file)
+        return false;
+
+    try
+    {
+        std::string line;
+        while (std::getline(file, line))
+        {
+            std::stringstream ss(line);
+
+            std::vector<std::string> tokens;
+            std::string token;
+            while (std::getline(ss, token, ' '))
+            {
+                tokens.push_back(token);
+            }
+
+            PathsAndFileExt tmp;
+            size_t tokenCount = tokens.size();
+            if (tokenCount == SLEEP_TOKEN_COUNT)
+            {
+                sleepDuration = std::stoi(tokens.at(0));
+            }
+            else if (tokenCount == PATHS_TOKEN_COUNT)
+            {
+                tmp.pathFrom = tokens[0];
+                tmp.pathTo = tokens[1];
+                tmp.ext = tokens[2];
+                pathsAndFileExt.push_back(tmp);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+    catch (...)
+    {
+        return false;
+    }
+
+    if (pathsAndFileExt.empty())
+        return false;
+
+    curIterator = pathsAndFileExt.begin();
+    configReaded = true;
+
+    return true;
+}
+
+bool Config::next()
+{
+    curIterator++;
+    if (curIterator == pathsAndFileExt.end())
+    {
+        curIterator = pathsAndFileExt.begin();
+        return false;
+    }
+
+    return true;
+}
+
+std::string Config::getFromPath() const
+{
+    return (*curIterator).pathFrom;
+}
+
+std::string Config::getToPath() const
+{
+    return (*curIterator).pathTo;
+}
+
+std::string Config::getFileExt() const
+{
+    return (*curIterator).ext;
+}
+
+unsigned Config::getSleepDuration() const
+{
+    return sleepDuration;
+}
+
+bool Config::isConfigReaded() const
+{
+    return configReaded;
+}
---
+++ Faizrahmanov.Artur/lab1/build.sh
@@ -0,0 +1,7 @@
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+cp ./build/FileMover .
+rm -r build
